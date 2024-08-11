5a85d1e1e767d0af683023ba8a5151c3ecd81ceb
Udith Kateel <udithshetty555@gmail.com>
2024-08-11 19:34:47 +0530

    first

--- PersonelTrainer/src/First.jsx
+++ PersonelTrainer/src/First.jsx
@@ -1,8 +1,12 @@
 import React from "react";
-
+import { Link,useNavigate } from "react-router-dom"; 
+import UserInfo from "./pages/UserInfo";
 export default function ImageComponent() {
   const openKFC=()=>{
-    window.open("https://online.kfc.co.in/")
+    window.location.href="https://online.kfc.co.in/";
+  }
+  const GotoUserInfo=()=>{
+    navigate("/UserInfo")
   }
   return (
     <main className="goggins">
@@ -13,7 +17,8 @@ export default function ImageComponent() {
         <button className="overlay-button" 
         onClick={openKFC}>Choose</button>
         <h1>You Decide</h1>
-        <button className="overlay-button">Choose</button>
+        <Link to ="/UserInfo">
+        <button className="overlay-button" >Choose</button></Link>
       </div>
     </main>
   );
--- PersonelTrainer/src/main.jsx
+++ PersonelTrainer/src/main.jsx
@@ -2,13 +2,25 @@ import React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route
+} from "react-router-dom";
 import App from './App.jsx';
 import './index.css';
+import UserInfo from './pages/UserInfo.jsx';
+  
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />},
+    {
+      path:"/UserInfo",
+      element:<UserInfo/>
+    }
+  ])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+  <RouterProvider router={router}/>
 );
